42610cee286d9f9916db5afd28231f639ac7d664
Tagg02 <harrison.taggart@hotmail.com>
2026-01-03 11:13:34 +0000

    Initial port scanner script structure

---
+++ port_scanner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Simple TCP port scanner (educational).
+Only scan systems you own or have explicit permission to test.
+"""
+
+def main() -> None:
+    print("Port Scanner (WIP)")
+
+if __name__ == "__main__":
+    main()
